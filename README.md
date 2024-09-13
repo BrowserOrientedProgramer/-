83d5dc1080a8edc858952132a3a627e13ed7322f
Ha? <101635709+BrowserOrientedProgramer@users.noreply.github.com>
2024-09-13 22:27:16 +0800

    实现基本语音交互功能

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# -
-SenseVoice实现音频转文字，Ollama Qwen2模型生成交流文本，ChatTTS实现文本转语音
+# ChatBot
+
+## 介绍
+
+ChatBot是基于Python的开放源代码聊天机器人框架，它可以实现文本对话、语音对话、语音合成、语音识别等功能。
+
+## 功能
+[SenseVoice](https://github.com/FunAudioLLM/SenseVoice)实现音频转文字，[Ollama](https://github.com/ollama/ollama) [Qwen2](https://ollama.com/library/qwen2)模型生成交流文本，[ChatTTS](https://github.com/2noise/ChatTTS)实现文本转语音
+
+## 使用方法
+下载项目
+```shell
+git clone https://github.com/BrowserOrientedProgramer/ChatBot.git
+cd ChatBot
+```
+安装依赖
+```shell
+conda create -n ChatBot python=3.10
+conda activate ChatBot
+pip install -r requirements.txt
+```
+
+提前开启Ollama
+```shell
+ollama serve
+```
+启动ChatBot
+```shell
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,117 @@
+import json
+import pyaudio
+import numpy as np
+import wave
+import requests
+from funasr import AutoModel
+from funasr.utils.postprocess_utils import rich_transcription_postprocess
+# NOTE: ollama must be running for this to work, start the ollama app or run `ollama serve`
+
+import ChatTTS
+import torch
+import torchaudio
+import playsound
+
+chat_machine = ChatTTS.Chat()
+chat_machine.load(compile=False) 
+
+audio_file = "temp_recording.wav"
+
+ollama_model = "qwen2:0.5b"
+ollama_url = "http://localhost:11434"
+
+ar_model_dir = "iic/SenseVoiceSmall"
+ar_model = AutoModel(
+    model=ar_model_dir,
+    trust_remote_code=True,
+    remote_code="./model.py",
+    vad_model="fsmn-vad",
+    vad_kwargs={"max_single_segment_time": 30000},
+    device="cuda:0",
+)
+
+def chat(messages):
+    r = requests.post(
+        ollama_url + "/api/chat",
+        json={"model": ollama_model, "messages": messages, "stream": True},
+    )
+    r.raise_for_status()
+    output = ""
+    for line in r.iter_lines():
+        body = json.loads(line)
+        if "error" in body:
+            raise Exception(body["error"])
+        if body.get("done") is False:
+            message = body.get("message", "")
+            content = message.get("content", "")
+            output += content
+            # the response streams one token at a time, print that as we receive it
+            print(content, end="", flush=True)
+        if body.get("done", False):
+            message["content"] = output
+            return message
+        
+def detect_silence(data, threshold=1000, chunk_size=1024):
+    audio_data = np.frombuffer(data, dtype=np.int16)
+    return np.mean(np.abs(audio_data)) < threshold
+
+# Function to record audio from the microphone and save to a file
+def record_audio(file_path, silence_threshold=512, silence_duration=4.0, chunk_size=1024):
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=chunk_size)
+    frames = []
+    print("Recording...")
+    silent_chunks = 0
+    speaking_chunks = 0
+    while True:
+        data = stream.read(chunk_size)
+        frames.append(data)
+        if detect_silence(data, threshold=silence_threshold, chunk_size=chunk_size):
+            silent_chunks += 1
+            if silent_chunks > silence_duration * (16000 / chunk_size):
+                break
+        else:
+            silent_chunks = 0
+            speaking_chunks += 1
+        if speaking_chunks > silence_duration * (16000 / chunk_size) * 10:
+            break
+    print("Recording stopped.")
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    wf = wave.open(file_path, 'wb')
+    wf.setnchannels(1)
+    wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
+    wf.setframerate(16000)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+def main():
+    messages = []
+    while True:
+        record_audio(audio_file)
+
+        res = ar_model.generate(
+            input=audio_file,
+            cache={},
+            language="auto",  # "zh", "en", "yue", "ja", "ko", "nospeech"
+            use_itn=True,
+            batch_size_s=60,
+            merge_vad=True,  #
+            merge_length_s=15,
+        )
+        user_input = rich_transcription_postprocess(res[0]["text"])
+        print(user_input)
+        if not user_input:
+            exit()
+        print()
+        messages.append({"role": "user", "content": user_input})
+        message = chat(messages)
+        wavs = chat_machine.infer(message["content"])
+        torchaudio.save("output.wav", torch.from_numpy(wavs[0]), 24000)
+        playsound.playsound("output.wav")
+        messages.append(message)
+        print("\n\n")
+        
+if __name__ == "__main__":
+    main()
